ef82f4b080130130769967d283d68b638287cedb
spoorti <spoortinaik99@gmail.com>
2024-04-27 11:22:17 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -95,6 +95,10 @@ app.delete('/clear-data', (req, res) => {
         });
 });
 
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
