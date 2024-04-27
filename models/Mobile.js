8d514a7cbf61c49946e9f52ec1f23d4a7b2f6c3a
spoorti <spoortinaik99@gmail.com>
2024-04-27 10:55:05 +0530

    some changes

--- models/Mobile.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const mobileSchema = new mongoose.Schema({
-    title: String,
-    price: String,
-    image: String
-});
-
-module.exports = mongoose.model('Mobile', mobileSchema);
